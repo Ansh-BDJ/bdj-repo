949c5ba81e9f82140b5748d2a1a78081aaaefdf6
Ansh-BDJ <anshbhardwaj3000@gmail.com>
2025-03-21 16:07:17 +0000

    datatype conversion

---
+++ 01_basics/02_typeConversion.js
@@ -0,0 +1,18 @@
+/*let score = " "
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+*/
+
+let IsLoggedIn = "a"
+let booleanIsLoggedIn = Boolean(IsLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+let someNumber = 33
+let stringSomeNumber = String(someNumber)
+console.log(stringSomeNumber);
