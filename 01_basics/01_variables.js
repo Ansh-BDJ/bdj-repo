f9140bb1ef3211a67b5a67d0e56972bdeb475258
Ansh-BDJ <anshbhardwaj3000@gmail.com>
2025-03-21 16:08:46 +0000

    datatype conversion

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 144553
+let accountEmail  = "anshbdj@gmail.com"
+var acccountPassword = "12345"
+accountCity = "Delhi"
+let accountState
+
+//accountId = 12344
+//console.log(accountId);
+
+console.table([accountId, acccountPassword, accountEmail, accountCity, accountState]);
