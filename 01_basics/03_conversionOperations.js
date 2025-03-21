92328a8c02892b3fd4a25eb801ec889b1fd704cf
Ansh-BDJ <anshbhardwaj3000@gmail.com>
2025-03-21 16:54:17 +0000

    conversion of operations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,10 @@
+let str1 = "hello"
+let str2 = " ansh"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "3");   
