43e5c7fa523205ba41d29beeed746ae0e42610be
Ansh-BDJ <anshbhardwaj3000@gmail.com>
2025-03-21 17:49:20 +0000

    comparison of datatypes

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,10 @@
+//console.log(undefined==0);
+//console.log(undefined>0);
+//console.log(undefined>=0);
+
+//console.log("2" === 2);
+
+const id = Symbol('123')
+const anotherId  = Symbol('123')
+console.log(id == anotherId);
+
